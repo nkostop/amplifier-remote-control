0bd67db56d7df99333bdecb196ebc9fe598ba7e3
Nikolaos Kostopoulos <nkostop@gmail.com>
2021-04-25 14:25:52 +0300

    Uploaded coefficients for B57045K 2904 10K thermistor

--- amplifier_control.ino
+++ amplifier_control.ino
@@ -55,7 +55,7 @@ int Vo1;
 int Vo2;
 float R1 = 10000;
 float logR2, R2, T1, T2;
-float c1 = 1.009249522e-03, c2 = 2.378405444e-04, c3 = 2.019202697e-07;
+float c1 = 1.306013916e-03, c2 = 2.136446243e-04, c3 = 1.035851727e-07;
 
 // the setup function runs once when you press reset or power the board
 void setup() {
